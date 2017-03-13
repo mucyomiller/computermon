c12ecb706dacc123ec6c9426a79ff762e1445a57
Mucyo M Fred <mucyomiller@gmail.com>
2017-03-14 00:49:24 +0200

    fix temporary error

---
+++ src/computermon/HandleLogin.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computermon;
+
+
+/**
+ *
+ * @author admin
+ */
+public class HandleLogin {
+    
+    String email;
+    String password;
+
+    public HandleLogin(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+    
+    public void  login(){   
+    }
+}
+    
